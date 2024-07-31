7a846210b5dd68401a817176e4e7d76c264e80d3
Peter Phan <62064025+phanthang1994@users.noreply.github.com>
2024-07-31 07:53:10 +0700

    a

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -188,53 +188,53 @@ import { useEffect, useState } from "react";
 //   )
 // }
 
-const lessons = [
-  {
-    id: 1,
-    name: "ReactJS là gì? Tại sao nên học ReactJS?",
-  },
-  {
-    id: 2,
-    name: "SPA/MPA là gì?",
-  },
-  {
-    id: 3,
-    name: "Arrow function",
-  },
-];
+// const lessons = [
+//   {
+//     id: 1,
+//     name: "ReactJS là gì? Tại sao nên học ReactJS?",
+//   },
+//   {
+//     id: 2,
+//     name: "SPA/MPA là gì?",
+//   },
+//   {
+//     id: 3,
+//     name: "Arrow function",
+//   },
+// ];
 
 
 
-function Content() {
-  const [lessonId, setLessoId] = useState(1);
+// function Content() {
+//   const [lessonId, setLessoId] = useState(1);
 
-  useEffect(() => {
-    const handleComment = ({ detail }) => {
-      console.log(detail);
-    };
-    window.addEventListener(`lesson-${lessonId}`, handleComment);
-    return () => {
-      window.removeEventListener(`lesson-${lessonId}`, handleComment);
-    };
-  }, [lessonId]);
+//   useEffect(() => {
+//     const handleComment = ({ detail }) => {
+//       console.log(detail);
+//     };
+//     window.addEventListener(`lesson-${lessonId}`, handleComment);
+//     return () => {
+//       window.removeEventListener(`lesson-${lessonId}`, handleComment);
+//     };
+//   }, [lessonId]);
 
-  return (
-    <div>
-      <ul>
-        {lessons.map((lesson) => (
-          <li
-            key={lesson.id}
-            style={{
-              color: lessonId === lesson.id ? "red" : "#333",
-            }}
-            onClick={() => setLessoId(lesson.id)}
-          >
-            {lesson.name}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <ul>
+//         {lessons.map((lesson) => (
+//           <li
+//             key={lesson.id}
+//             style={{
+//               color: lessonId === lesson.id ? "red" : "#333",
+//             }}
+//             onClick={() => setLessoId(lesson.id)}
+//           >
+//             {lesson.name}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
 export default Content;
