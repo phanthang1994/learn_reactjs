038d4a3861471267fd0c47de54535285151b57bf
Peter Phan <62064025+phanthang1994@users.noreply.github.com>
2024-08-04 11:15:21 +0700

    add ref

--- contex_usereducer/src/App.js
+++ contex_usereducer/src/App.js
@@ -1,19 +1,23 @@
 import {actions, useStore } from './store'
 import TodoList from './store/todo_list';
+import { useRef } from 'react';
 function App() {
 
   const [state, dispatch] = useStore()
   // console.log(state);
   const {todos, todoInput} = state;
-  
+  const ref = useRef()
   function handleAddTodo() {
     dispatch(actions.addTodo(todoInput))
+    dispatch(actions.setTodoInput(''))
+    ref.current.focus() // focus on input field when add todo button is clicked  // ref.current.focus() will set focus on the input field when the component is rendered for the first time.
   }
-
+ 
   // console.log("todos: ", todos);
   return (
     <div>
       <input 
+        ref={ref}
         value={todoInput}
         placeholder='Enter todo...'
         onChange={
